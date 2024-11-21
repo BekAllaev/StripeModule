9a9cbdecada4d2b90bbcf465a245ed66616f7c39
Bekzod Allaev <Allaev_@outlook.com>
2024-11-21 17:00:58 +0100

    Add project files.

---
+++ StripeModule.Backend/IPayment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StripeModule.Backend
+{
+    public interface IPayment
+    {
+        public Guid Id { get; }
+
+        /// <summary>
+        /// Product/service id for which payment happened
+        /// </summary>
+        public Guid ProductId { get; }
+
+        public decimal Price { get; }
+
+        public DateTime PaymentDateTime { get; }
+    }
+}
